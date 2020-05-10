b504b974e62c8d38765910415d4f8ae4882c8b43
trevor-shepard <trevorbshepard@gmail.com>
2020-05-10 18:20:16 -0500

    Cell container styling

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -7,15 +7,33 @@ type CellProps = {
     handleClick(): void
 }
 
-const Cell: React.FunctionComponent<CellProps> = ({ alive, handleClick, history }) => (
-    <CellContainer  onClick={handleClick}>
-        {alive ? "X" : history ? "#" : "O"}
-    </CellContainer>
-)
+const Cell: React.FunctionComponent<CellProps> = ({ alive, handleClick, history }) => {
+    
+    
+    return alive ? (<AliveContainer  onClick={handleClick} /> ) : history ? (<HistoryContainer  onClick={handleClick} /> ) : (<DeadContainer  onClick={handleClick} /> )
+       
 
+}
 const CellContainer = styled('div')` 
     padding: 5px;
+    height: 5px;
+    width: 5px;
+    border: 1px solid black;
+    border-radius: 3px;
+    font-size: 10px;
+    margin: 5px;
 `
 
+const AliveContainer = styled(CellContainer)`
+    border-color: blue;
+`
+
+const DeadContainer = styled(CellContainer)`
+    border-color: red
+`
+
+const HistoryContainer = styled(CellContainer)`
+    border-color: yellow
+`
 
 export default Cell
